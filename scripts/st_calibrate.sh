ceee3d463c1bb4a0a4d33f0794ded75d5c15845c
fmederos <fmederos@gmail.com>
2017-08-28 21:04:23 -0300

    Posibility to calibrate with Nth fits image

--- scripts/st_calibrate.sh
+++ scripts/st_calibrate.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+if [ -z $1 ]; then
+    N=10
+else
+		echo "Using image $1 to calibrate"
+    N=$1
+		rm test.fits
+fi
+
 if [ ! -e "test.fits" ]; then
-    ls -1 2*.fits | head -n10 | tail -n1 | awk '{printf("cp %s test.fits\n",$1)}' | sh
+    ls -1 2*.fits | head -n$N | tail -n1 | awk '{printf("cp %s test.fits\n",$1)}' | sh
 fi
 sextractor test.fits -c $ST_DATADIR/sextractor/default.sex
 mv test.cat test.fits.cat
