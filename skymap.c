8b4fa0e67eba4a6353d62968cca4df924d5bc23a
fmederos <fmederos@gmail.com>
2017-11-08 00:33:04 -0300

    Add 'All But GEO' orbit filter.

--- skymap.c
+++ skymap.c
@@ -1608,6 +1608,8 @@ void skymap_plotsatellite(char *filename,int satno,double mjd0,double dt)
       continue;
     if (m.leoflag==4 && strcmp(type,"GTO")!=0)
       continue;
+    if (m.leoflag==5 && strcmp(type,"GEO")==0)
+      continue;
 
     sprintf(norad," %ld",Isat);
 
@@ -2352,6 +2354,8 @@ int plot_skymap(void)
 	    sprintf(text,"GEO");
 	  else if (m.leoflag==4)
 	    sprintf(text,"GTO");
+	  else if (m.leoflag==5)
+	    sprintf(text,"AllButGEO");
 	} else if (Isatsel>0) {
 	  sprintf(text,"%05d",(int) Isatsel);
 	} else {
@@ -2602,7 +2606,7 @@ int plot_skymap(void)
     if (c=='L') {
       if (Isatsel==0) {
 	m.leoflag++;
-	if (m.leoflag>4)
+	if (m.leoflag>5)
 	  m.leoflag=-1;
 	redraw=1;
       } else {
